18eace790f1e6d3614faf5dd370e8da23719cb43
hicham68 <71725850+hicham68@users.noreply.github.com>
2021-11-01 22:32:46 +0100

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -72,6 +72,30 @@ tabs.forEach(tab =>{
 })
 
 /*==================== SERVICES MODAL ====================*/
+const modalViews = document.querySelectorAll('.services__modal'),
+// modalBtns sera un tableau avec plusieur button qui contiennent la classe correspondante
+modalBtns = document.querySelectorAll('.services__button'), 
+modalCloses = document.querySelectorAll('.services__modal-close')
+
+let modal = function(indexModalClick){
+    // console.log(modalViews[indexModalClick])
+    modalViews[indexModalClick].classList.add('active-modal')
+}
+
+
+modalBtns.forEach((modalBtn, i) =>{
+    modalBtn.addEventListener('click', () =>{
+        modal(i)
+    })
+})
+
+modalCloses.forEach(modalClose =>{
+    modalClose.addEventListener('click', () =>{
+        modalViews.forEach(modalView =>{
+            modalView.classList.remove('active-modal')
+        })
+    })
+})
 
 
 /*==================== PORTFOLIO SWIPER  ====================*/
